08ae23dbbc22b7958423b85e5a5d5ae6d94bd138
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-01 21:48:03 -0700

    added client

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container-fluid">
+      <h1>TEST</h1>
+      <Square></Square>
+    </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => alert('click')}>
+        {this.props.value}
+      </button>
+    );
+  }
+ }
+
+export default App;
