b2a3426f4d3b0a5fc24aded7b4eddfd5cb89a7f8
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-06 13:30:18 -0700

    added more static files

---
+++ client/build/precache-manifest.2538195145f28b7cb24485e002d59780.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293e4d2e449d116a26a346a18c9aa484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18b6e4e894d90e91662",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ce3338590c3b5f2e13f1",
+    "url": "/static/css/main.a28e104c.chunk.css"
+  },
+  {
+    "revision": "d18b6e4e894d90e91662",
+    "url": "/static/js/2.0ab70eb8.chunk.js"
+  },
+  {
+    "revision": "ce3338590c3b5f2e13f1",
+    "url": "/static/js/main.30c3c1fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c177345c4a08791a36e6856dc5c14579",
+    "url": "/static/media/BoschwitzElliot-Resume.c177345c.pdf"
+  },
+  {
+    "revision": "a59066dd6f9c281b11b8292424f7d4d7",
+    "url": "/static/media/HelicopterGame.a59066dd.jar"
+  },
+  {
+    "revision": "80a9b3abbddd6119201f88d4b8ea12c4",
+    "url": "/static/media/LobWriteup.80a9b3ab.pdf"
+  },
+  {
+    "revision": "4e06f2187b65a4f54f978b6d116a82ed",
+    "url": "/static/media/SafetyPennDocumentation.4e06f218.pdf"
+  },
+  {
+    "revision": "b05de98b058a4f1493de1971387da453",
+    "url": "/static/media/rainier.b05de98b.jpg"
+  },
+  {
+    "revision": "7305bb1b69ab9b03cf036646913172e7",
+    "url": "/static/media/sp_1.7305bb1b.png"
+  },
+  {
+    "revision": "887da467126ee34e66b60610e71b81b0",
+    "url": "/static/media/sp_2.887da467.png"
+  },
+  {
+    "revision": "ed94bf124ad6610d4ec11c9ec712aa5a",
+    "url": "/static/media/sp_3.ed94bf12.png"
+  },
+  {
+    "revision": "3c29baa5ebf648234f31a78aa54e9ef2",
+    "url": "/static/media/sp_4.3c29baa5.png"
+  },
+  {
+    "revision": "d75ce39be6e89df74be01f380f5deddb",
+    "url": "/static/media/sp_5.d75ce39b.png"
+  }
+]);
