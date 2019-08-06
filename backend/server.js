81c7b23cab9a3eea89af704bbbf1a88bab6d56f8
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-05 20:49:39 -0700

    get request response change

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const path = require('path');
 
-const API_PORT = 3001;
+const API_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', (req, res) => {
+	res.send('just gonna send it');
 });
 
 // launch our backend into a port
