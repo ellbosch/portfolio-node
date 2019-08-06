2a88c2b3b6a1d8dba8bc87b15d7a719aec4a1f1d
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-05 23:01:52 -0700

    added index to server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const API_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('just gonna send it');
-});
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
