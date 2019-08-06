025549067cc821305bbaa95398c08034ff05a8a6
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-05 20:43:15 -0700

    modified server file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const API_PORT = 3001;
 const app = express();
-const router = express.Router();
 
-router.get('/');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
